3cfa56026610dd7f26f6f7584068841bd840f3f9
Lynn <zzzzzzzl@tamu.edu>
2026-01-02 12:31:24 -0600

    fix: 新增菜品

--- sky-server/src/main/java/com/sky/mapper/DishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/DishMapper.java
@@ -1,17 +1,21 @@
 package com.sky.mapper;
 import java.util.List;
 
+import com.sky.annotation.AutoFill;
 import com.sky.entity.Dish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 
 import org.apache.ibatis.annotations.Select;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Mapper
 public interface DishMapper {
 
     Long countByCategoryId(@Param("categoryId")Long categoryId);
 
+    @AutoFill(OperationType.INSERT)
     int insert(Dish dish);
 
 
