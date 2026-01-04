d8bfbec4fa9d9edd053ccd64ef4a490a409b489a
Lynn <zzzzzzzl@tamu.edu>
2026-01-04 01:18:20 -0600

    feat: 图片上传到阿里云

--- sky-server/src/main/java/com/sky/controller/admin/CommonController.java
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -2,9 +2,11 @@ package com.sky.controller.admin;
 
 import com.sky.constant.MessageConstant;
 import com.sky.result.Result;
+import com.sky.utils.AliOssUtil;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,37 +24,73 @@ import java.util.UUID;
 @Slf4j
 public class CommonController {
 
-    @Value("${sky.path.upload-dir}")
-    private String uploadDir;
+    @Autowired
+    private AliOssUtil aliOssUtil;
 
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
     @PostMapping("/upload")
-    @ApiOperation("File upload")
-    public Result<String> upload(MultipartFile file) {
-        log.info("upload file with path: {}", uploadDir);
+    @ApiOperation("文件上传")
+    public Result<String> upload(MultipartFile file){
+        log.info("文件上传：{}",file);
+
         try {
-            // 1. 获取原始文件名
+            //原始文件名
             String originalFilename = file.getOriginalFilename();
-
-            // 2. 截取后缀
+            //截取原始文件名的后缀   dfdfdf.png
             String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            //构造新文件名称
+            String objectName = UUID.randomUUID().toString() + extension;
 
-            // 3. 构造新文件名
-            String newFileName = UUID.randomUUID().toString() + extension;
-
-            // 4. 检查目录是否存在
-            File dir = new File(uploadDir);
-            if (!dir.exists()) {
-                dir.mkdirs();
-            }
-
-            // 5. 保存文件
-            file.transferTo(new File(dir, newFileName));
-
-            // 6. 返回文件路径
-            return Result.success(newFileName);
+            //文件的请求路径
+            String filePath = aliOssUtil.upload(file.getBytes(), objectName);
+            return Result.success(filePath);
         } catch (IOException e) {
-            return Result.error(MessageConstant.UPLOAD_FAILED);
+            log.error("文件上传失败：{}", e);
         }
 
+        return Result.error(MessageConstant.UPLOAD_FAILED);
     }
+
+
+//
+//    @Value("${sky.path.upload-dir}")
+//    private String uploadDir;
+//
+//    @PostMapping("/upload")
+//    @ApiOperation("File upload")
+//    public Result<String> upload(MultipartFile file) {
+//        log.info("upload file with path: {}", uploadDir);
+//        try {
+//            // 1. 获取原始文件名
+//            String originalFilename = file.getOriginalFilename();
+//
+//            // 2. 截取后缀
+//            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+//
+//            // 3. 构造新文件名
+//            String newFileName = UUID.randomUUID().toString() + extension;
+//
+//            // 4. 检查目录是否存在
+//            File dir = new File(uploadDir);
+//            if (!dir.exists()) {
+//                dir.mkdirs();
+//            }
+//
+//            // 5. 保存文件
+//            file.transferTo(new File(dir, newFileName));
+//
+//            // 6. 返回文件路径
+//            return Result.success(newFileName);
+//        } catch (IOException e) {
+//            return Result.error(MessageConstant.UPLOAD_FAILED);
+//        }
+//
+//    }
+
+
+
 }
